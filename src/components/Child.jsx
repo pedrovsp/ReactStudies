4f3f9811433f149f11fda364619416b0a751e2bb
Pedro Vitorino <pedro.vitorinosp@gmail.com>
2020-05-14 23:31:10 -0300

    comunicação filho para pai

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export default (props) => {
-    return <div>{props.name} {props.familyName}</div>
+    return (
+        <div>
+            <span>{props.name} {props.familyName}</span>
+            <button onClick={() => props.whoCalls(props.name)}>Call parent</button>
+        </div>)
 }
--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { childrenProps } from '../utils/react-utils'
 
-export default (props) => 
+export default (props) => {
+    let customProps = {...props};
+    customProps.whoCalls = (name) => {
+        alert('Hello father, it is ' + name);
+    }
+
+    return (
         <div>
             <h3>Family { props.familyName }</h3>
-            { childrenProps(props) }
-        </div>
+            { childrenProps(customProps) }
+        </div>)
+}
