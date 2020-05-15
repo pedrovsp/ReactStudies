15331e0b23cf8a0ead3ada5c2a4080cb1c7f8d72
Pedro Vitorino <pedro.vitorinosp@gmail.com>
2020-05-14 23:42:05 -0300

    useState to change parent component

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 export default (props) => {
+    const age = () => Math.random() * 20 + 30
     return (
         <div>
             <span>{props.name} {props.familyName}</span>
-            <button onClick={() => props.whoCalls(props.name)}>Call parent</button>
+            <button onClick={() => props.whoCalls(props.name, () => age())}>Call parent</button>
         </div>)
 }
--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { childrenProps } from '../utils/react-utils'
+import If from '../utils/If'
 
 export default (props) => {
     let customProps = {...props};
-    customProps.whoCalls = (name) => {
-        alert('Hello father, it is ' + name);
+    const [name, setWhoCalled] = useState('')
+    const [age, setCallerAge] = useState(0)
+
+    customProps.whoCalls = (name, age) => {
+        setWhoCalled(name);
+        setCallerAge(age)
     }
 
     return (
         <div>
             <h3>Family { props.familyName }</h3>
+            <If condition={name !== ''}>
+                <h5>{name} called, he is {age} years old</h5>
+            </If>
             { childrenProps(customProps) }
         </div>)
 }
